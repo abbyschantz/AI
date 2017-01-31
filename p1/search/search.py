1c84df8e04dd2d440f368eb140372e177f71f210
Abigail Schantz <aschantz@gardenia.cs.hmc.edu>
2017-01-30 17:55:53 -0800

    updates to DFS for search.py

--- p1/search/search.py
+++ p1/search/search.py
@@ -1,4 +1,5 @@
 # search.py
+
 # ---------
 # Licensing Information:  You are free to use or extend these projects for
 # educational purposes provided that (1) you do not distribute or publish
@@ -21,122 +22,126 @@ import util
 import pacman
 
 class SearchProblem:
-    """
-    This class outlines the structure of a search problem, but doesn't implement
-    any of the methods (in object-oriented terminology: an abstract class).
+	"""
+	This class outlines the structure of a search problem, but doesn't implement
+	any of the methods (in object-oriented terminology: an abstract class).
 
-    You do not need to change anything in this class, ever.
-    """
+	You do not need to change anything in this class, ever.
+	"""
 
-    def getStartState(self):
-        """
-        Returns the start state for the search problem.
-        """
-        util.raiseNotDefined()
+	def getStartState(self):
+		"""
+		Returns the start state for the search problem.
+		"""
+		util.raiseNotDefined()
 
-    def isGoalState(self, state):
-        """
-          state: Search state
+	def isGoalState(self, state):
+		"""
+		  state: Search state
 
-        Returns True if and only if the state is a valid goal state.
-        """
-        util.raiseNotDefined()
+		Returns True if and only if the state is a valid goal state.
+		"""
+		util.raiseNotDefined()
 
-    def getSuccessors(self, state):
-        """
-          state: Search state
+	def getSuccessors(self, state):
+		"""
+		  state: Search state
 
-        For a given state, this should return a list of triples, (successor,
-        action, stepCost), where 'successor' is a successor to the current
-        state, 'action' is the action required to get there, and 'stepCost' is
-        the incremental cost of expanding to that successor.
-        """
-        util.raiseNotDefined()
+		For a given state, this should return a list of triples, (successor,
+		action, stepCost), where 'successor' is a successor to the current
+		state, 'action' is the action required to get there, and 'stepCost' is
+		the incremental cost of expanding to that successor.
+		"""
+		util.raiseNotDefined()
 
-    def getCostOfActions(self, actions):
-        """
-         actions: A list of actions to take
+	def getCostOfActions(self, actions):
+		"""
+		 actions: A list of actions to take
 
-        This method returns the total cost of a particular sequence of actions.
-        The sequence must be composed of legal moves.
-        """
-        util.raiseNotDefined()
+		This method returns the total cost of a particular sequence of actions.
+		The sequence must be composed of legal moves.
+		"""
+		util.raiseNotDefined()
 
 
 def tinyMazeSearch(problem):
-    """
-    Returns a sequence of moves that solves tinyMaze.  For any other maze, the
-    sequence of moves will be incorrect, so only use this for tinyMaze.
-    """
-    from game import Directions
-    s = Directions.SOUTH
-    w = Directions.WEST
-    return  [s, s, w, s, w, w, s, w]
+	"""
+	Returns a sequence of moves that solves tinyMaze.  For any other maze, the
+	sequence of moves will be incorrect, so only use this for tinyMaze.
+	"""
+	from game import Directions
+	s = Directions.SOUTH
+	w = Directions.WEST
+	return  [s, s, w, s, w, w, s, w]
 
 def depthFirstSearch(problem):
-    """
-    Search the deepest nodes in the search tree first.
+	"""
+	Search the deepest nodes in the search tree first.
 
-    Your search algorithm needs to return a list of actions that reaches the
-    goal. Make sure to implement a graph search algorithm.
+	Your search algorithm needs to return a list of actions that reaches the
+	goal. Make sure to implement a graph search algorithm.
 
-    To get started, you might want to try some of these simple commands to
-    understand the search problem that is being passed in:
+	To get started, you might want to try some of these simple commands to
+	understand the search problem that is being passed in:
 
-    print "Start:", problem.getStartState()
-    print "Is the start a goal?", problem.isGoalState(problem.getStartState())
-    print "Start's successors:", problem.getSuccessors(problem.getStartState())
-    """
-    "*** YOUR CODE HERE ***"
-    from game import Directions
-    closed = {}
+	print "Start:", problem.getStartState()
+	print "Is the start a goal?", problem.isGoalState(problem.getStartState())
+	print "Start's successors:", problem.getSuccessors(problem.getStartState())
+	"""
+	"*** YOUR CODE HERE ***"
+	#working on the implementation of the actionlist; currently printing a list of actions that is incorrect
 
-    """fringe is a stack of current pacman states in the format (pos, direction)"""
-    fringe = util.Stack.__init__()
-    while not fringe.isEmpty(self):
+	closed = set()
 
-        (x,y) = pacman.getPacmanPosition()
-        """goal test"""
-        if pacman.hasFood(x, y):
-            return fringe
+	fringe = util.Stack()
+	startState = problem.getStartState()
+	fringe.push(startState)
 
-        (currPacman) = pacman.getPacmanState()
-        if not closed.has_key(currPacman):
-            closed.put(currPacman)
-            futureDirections = pacman.PacmanRules.getLegalActions()
-            (currX, currY) = currPacman.getPosition()
-            for futureDirect in futureDirections:
-                if futureDirect == North:
-                    fringe.push(AgentState.__init__(configuration.__init__((currX, currY+1), North), True))
-                elif futureDirect == West:
-                    fringe.push(AgentState.__init__(configuration.__init__((currX-1, currY), West), True))
-                elif futureDirect == South:
-                    fringe.push(AgentState.__init__(configuration.__init__((currX, currY-1), South), True))
-                elif futureDirect == East:
-                    fringe.push(AgentState.__init__(configuration.__init__((currX+1, currY), East), True))
-    util.raiseNotDefined()
+	trackingList = []
+	trackingList.append(startState)
+
+	actionList = []
+
+	while not fringe.isEmpty():
+		curr = fringe.pop()
+
+		trackingList.pop()
+		trackingList.append(curr)
+
+
+		if problem.isGoalState(curr):
+			return actionList
+		if not (curr in closed):
+			closed.add(curr)
+			succList = problem.getSuccessors(curr)
+			for (successor, action, cost) in succList:
+				fringe.push(successor)
+
+	util.raiseNotDefined()
+
+
 
 def breadthFirstSearch(problem):
-    """Search the shallowest nodes in the search tree first."""
-    "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+	"""Search the shallowest nodes in the search tree first."""
+	"*** YOUR CODE HERE ***"
+	util.raiseNotDefined()
 
 def uniformCostSearch(problem):
-    """Search the node of least total cost first."""
-    "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+	"""Search the node of least total cost first."""
+	"*** YOUR CODE HERE ***"
+	util.raiseNotDefined()
 
 def nullHeuristic(state, problem=None):
-    """
-    A heuristic function estimates the cost from the current state to the nearest
-    goal in the provided SearchProblem.  This heuristic is trivial.
-    """
-    return 0
+	"""
+	A heuristic function estimates the cost from the current state to the nearest
+	goal in the provided SearchProblem.  This heuristic is trivial.
+	"""
+	return 0
 
 def aStarSearch(problem, heuristic=nullHeuristic):
-    """Search the node that has the lowest combined cost and heuristic first."""
-    "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+	"""Search the node that has the lowest combined cost and heuristic first."""
+	"*** YOUR CODE HERE ***"
+	util.raiseNotDefined()
 
 
 # Abbreviations
