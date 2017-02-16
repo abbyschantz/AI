6d55716842505c3f33e8577f0a6645ff55a58819
abbyschantz <abby.schantz@gmail.com>
2017-02-15 21:31:07 -0800

    updates to q1

--- p2/multiagent/multiAgents.py
+++ p2/multiagent/multiAgents.py
@@ -19,174 +19,157 @@ import random, util
 from game import Agent
 
 class ReflexAgent(Agent):
-	"""
-	  A reflex agent chooses an action at each choice point by examining
-	  its alternatives via a state evaluation function.
-
-	  The code below is provided as a guide.  You are welcome to change
-	  it in any way you see fit, so long as you don't touch our method
-	  headers.
-	"""
-
-
-	def getAction(self, gameState):
-		"""
-		You do not need to change this method, but you're welcome to.
-
-		getAction chooses among the best options according to the evaluation function.
-
-		Just like in the previous project, getAction takes a GameState and returns
-		some Directions.X for some X in the set {North, South, West, East, Stop}
-		"""
-		# Collect legal moves and successor states
-		legalMoves = gameState.getLegalActions()
-
-		# Choose one of the best actions
-		scores = [self.evaluationFunction(gameState, action) for action in legalMoves]
-		bestScore = max(scores)
-		bestIndices = [index for index in range(len(scores)) if scores[index] == bestScore]
-		chosenIndex = random.choice(bestIndices) # Pick randomly among the best
-
-		"Add more of your code here if you want to"
-
-		return legalMoves[chosenIndex]
-
-	def evaluationFunction(self, currentGameState, action):
-		"""
-		Design a better evaluation function here.
-
-		The evaluation function takes in the current and proposed successor
-		GameStates (pacman.py) and returns a number, where higher numbers are better.
-
-		The code below extracts some useful information from the state, like the
-		remaining food (newFood) and Pacman position after moving (newPos).
-		newScaredTimes holds the number of moves that each ghost will remain
-		scared because of Pacman having eaten a power pellet.
-
-		Print out these variables to see what you're getting, then combine them
-		to create a masterful evaluation function.
-		"""
-		# Useful information you can extract from a GameState (pacman.py)
-		successorGameState = currentGameState.generatePacmanSuccessor(action)
-		newPos = successorGameState.getPacmanPosition()
-		newFood = successorGameState.getFood()
-		newGhostStates = successorGameState.getGhostStates()
-		newScaredTimes = [ghostState.scaredTimer for ghostState in newGhostStates]
-		newWalls = successorGameState.getWalls()
-
-		"*** YOUR CODE HERE ***"
-		"""keeping this as an idea: if ghost distance is less than some arb.
-		amount, turn around"""
-		(pacx, pacy) = newPos
-		ghostNextDoor = False
-		closestGhostDist = 9999999
-		for ghostState in newGhostStates:
-			(x,y) = ghostState.getPosition()
-			currDistance = manhattanDistance(newPos, (x,y))
-			if (abs(x - pacx) <= 4) and (abs(y - pacy) <= 4):
-				ghostNextDoor = True
-			if currDistance < closestGhostDist:
-				closestGhostDist = currDistance
-
-		#closest food record
-		foodPathList = breadthFirstSearch(currentGameState)
-
-
-		(foodx, foody) = 999999, 999999
-		closestFoodDist = 999999
-		listOfFood = newFood.asList()
-
-		for (x,y) in listOfFood:
-			manhattDistToCurr = manhattanDistance(newPos, (x,y))
-			if manhattDistToCurr < closestFoodDist:
-				(foodx, foody) = (x,y)
-				closestFoodDist = manhattDistToCurr
-
-
-		stateChange = successorGameState.getScore() - currentGameState.getScore()
-
-		if ghostNextDoor:
-			stateChange = stateChange - 500
-
-		if action == 'Stop':
-			stateChange = stateChange - 10
-		#if (pacx == foodx) and ((foodx - pacx) =
-
-		if newPos in foodPathList:
-			#print 'oh captain'
-			if len(foodPathList) <= 5:
-				stateChange = stateChange + (10/closestFoodDist)
-			else:
-				stateChange = stateChange + 1
-		else:
-			#deviate from path, when ghost is within 5
-			stateChange = stateChange - 20
-			#print 'captain'
-
-		#if ghost is scared and we're close enough, go towards the ghosts
-
-		#return stateChange
-		return 0
-
-def breadthFirstSearch(gameState):
-	"""Search the shallowest nodes in the search tree first."""
-	"*** YOUR CODE HERE ***"
-	queue = util.Queue()
-	return generalizedSearch(gameState, queue)
-	util.raiseNotDefined()
-
-def generalizedSearch(gameState, dataStruc):
-	closed = []
-	wallsList = gameState.getWalls().asList()
-	closed.extend(wallsList)
-
-	startList = []
-	startList.append(gameState.getPacmanPosition())
-	dataStruc.push(startList)
-	currFood = gameState.getFood()
-	currFoodList = currFood.asList()
-
-	while not dataStruc.isEmpty():
-		"""[(currx, curry), (x1,y1), (x2, y2), ..., (xn, yn)]"""
-		currAndActionsList = dataStruc.pop()
-		#print currAndActionsList
-		(currx, curry) = currAndActionsList[0]
-
-
-		if (currx, curry) in currFoodList:
-			path = currAndActionsList[1:len(currAndActionsList)]
-			path.append((currx, curry))
-			#print path[0]
-			return path
-
-		if not ((currx,curry) in closed):
-			closed.append((currx, curry))
-
-			succList = []
-			#adds immediate points around current point
-			if (currx + 1) < currFood.width:
-				new = (currx+1, curry)
-				succList.append(new)
-			if (currx - 1) >= 0:
-				new = (currx-1, curry)
-				succList.append(new)
-			if (curry + 1) < currFood.height:
-				new = (currx, curry+1)
-				succList.append(new)
-			if (curry - 1) >= 0:
-				new = (currx, curry-1)
-				succList.append(new)
-
-			for (xn, yn) in succList:
-
-				newList = []
-				newList.append((xn,yn))
-				#print newList
-				newList + currAndActionsList[1:len(currAndActionsList)]
-				dataStruc.push(newList)
-
-
-	util.raiseNotDefined()
+    """
+      A reflex agent chooses an action at each choice point by examining
+      its alternatives via a state evaluation function.
+ 
+      The code below is provided as a guide.  You are welcome to change
+      it in any way you see fit, so long as you don't touch our method
+      headers.
+    """
+ 
+ 
+    def getAction(self, gameState):
+        """
+        You do not need to change this method, but you're welcome to.
+ 
+        getAction chooses among the best options according to the evaluation function.
+ 
+        Just like in the previous project, getAction takes a GameState and returns
+        some Directions.X for some X in the set {North, South, West, East, Stop}
+        """
+        # Collect legal moves and successor states
+        legalMoves = gameState.getLegalActions()
+ 
+        # Choose one of the best actions
+        scores = [self.evaluationFunction(gameState, action) for action in legalMoves]
+        bestScore = max(scores)
+        bestIndices = [index for index in range(len(scores)) if scores[index] == bestScore]
+        chosenIndex = random.choice(bestIndices) # Pick randomly among the best
+ 
+        "Add more of your code here if you want to"
+ 
+        return legalMoves[chosenIndex]
+ 
+    def evaluationFunction(self, currentGameState, action):
+        """
+        Design a better evaluation function here.
+ 
+        The evaluation function takes in the current and proposed successor
+        GameStates (pacman.py) and returns a number, where higher numbers are better.
+ 
+        The code below extracts some useful information from the state, like the
+        remaining food (newFood) and Pacman position after moving (newPos).
+        newScaredTimes holds the number of moves that each ghost will remain
+        scared because of Pacman having eaten a power pellet.
+ 
+        Print out these variables to see what you're getting, then combine them
+        to create a masterful evaluation function.
+        """
+        # Useful information you can extract from a GameState (pacman.py)
+        successorGameState = currentGameState.generatePacmanSuccessor(action)
+        newPos = successorGameState.getPacmanPosition()
+        newFood = successorGameState.getFood()
+        newGhostStates = successorGameState.getGhostStates()
+        newScaredTimes = [ghostState.scaredTimer for ghostState in newGhostStates]
+        newWalls = successorGameState.getWalls()
+ 
+        "*** YOUR CODE HERE ***"
+        """keeping this as an idea: if ghost distance is less than some arb.
+        amount, turn around"""
+        if successorGameState.isWin():
+            return successorGameState.getScore()
+ 
+        if successorGameState.isLose():
+            return successorGameState.getScore()
+ 
+        ghostPositionsList = successorGameState.getGhostPositions()
+        foodList = newFood.asList()
+        capsuleList = successorGameState.getCapsules()
+        foodList.extend(capsuleList)
+        foodPathList = self.breadthFirstSearch(successorGameState, foodList)
+        ghostPathList = self.breadthFirstSearch(successorGameState, ghostPositionsList)
+        stateChange = successorGameState.getScore() - currentGameState.getScore()
+ 
+        if successorGameState.getPacmanPosition() in capsuleList:
+            return stateChange + 50
+ 
+        ghostNextDoor = False
+        if len(ghostPathList) < 6:
+            ghostNextDoor = True
+ 
+        if ghostNextDoor:
+            stateChange = stateChange - 250
+ 
+        if newPos in foodPathList:
+            #print 'oh captain'
+            stateChange = stateChange + (10/len(foodPathList))
+        else:
+            #deviate from path, when ghost is within 5
+            stateChange = stateChange - 20
+ 
+        #if ghost is scared and we're close enough, go towards the ghosts
+ 
+        #return stateChange
+        return stateChange
+ 
+    def breadthFirstSearch(self, gameState, goalList):
+        """Search the shallowest nodes in the search tree first."""
+        "*** YOUR CODE HERE ***"
+        queue = util.Queue()
+        return self.generalizedSearch(gameState, queue, goalList)
+        util.raiseNotDefined()
+ 
+    def generalizedSearch(self, gameState, dataStruc, goalList):
+        goalSet = set(goalList)
+ 
+        wallsMatr = gameState.getWalls()
+        wallsList = wallsMatr.asList()
+        closed = set(wallsList)
+ 
+        startList = []
+        startList.append(gameState.getPacmanPosition())
+        dataStruc.push(startList)
+ 
+ 
+        while not dataStruc.isEmpty():
+            """currAndActions[(currx, curry), (x1,y1), (x2, y2), ..., (xn, yn)]"""
+            currAndActions = dataStruc.pop()
+ 
+            (currx, curry) = currAndActions.pop(0)
+ 
+ 
+            if (currx, curry) in goalSet:
+                currAndActions.append((currx, curry))
+                print currAndActions
+                return currAndActions
+ 
+            if not ((currx,curry) in closed):
+                closed.add((currx, curry))
+ 
+                succList = []
+                #adds immediate points around current point
+                if (currx + 1) < wallsMatr.width and ((currx + 1), curry) not in closed:
+                    new = (currx+1, curry)
+                    succList.append(new)
+                if (currx - 1) >= 0 and ((currx - 1), curry) not in closed:
+                    new = (currx-1, curry)
+                    succList.append(new)
+                if (curry + 1) < wallsMatr.height and (currx, (curry + 1)) not in closed:
+                    new = (currx, curry+1)
+                    succList.append(new)
+                if (curry - 1) >= 0 and (currx, (curry - 1)) not in closed:
+                    new = (currx, curry-1)
+                    succList.append(new)
+ 
+                for (xn, yn) in succList:
+                    newList = []
+                    newList.append((xn,yn))
+                    currAndActions.append((currx,curry))
+                    newList.extend(currAndActions)
+                    dataStruc.push(newList)
+ 
+        util.raiseNotDefined()
 	
 def scoreEvaluationFunction(currentGameState):
 	"""
