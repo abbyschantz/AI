77d74b0b2032a960c2a387372b60d7a2d83d1bba
abbyschantz <abby.schantz@gmail.com>
2017-02-22 21:16:39 -0800

    tried pr1 but returning 0s

--- p3/reinforcement/valueIterationAgents.py
+++ p3/reinforcement/valueIterationAgents.py
@@ -62,6 +62,31 @@ class ValueIterationAgent(ValueEstimationAgent):
     def runValueIteration(self):
         # Write value iteration code here
         "*** YOUR CODE HERE ***"
+        #loop through each iteration
+        for i in range(self.iterations):
+          #loop through each state in the states of that interaction
+          for state in self.mdp.getStates():
+            #initialize utility and rewards to 0
+            utility = 0
+            reward = 0
+            #loop through each action of the possible actions for that state 
+            for action in self.mdp.getPossibleActions(state):
+              #set utility for this specific action to 0 
+              currUtility = 0
+              #loop through each of the state, prob pairs for the transition states
+              for nextStateAndProb in self.mdp.getTransitionStatesAndProbs(state, action):
+                nextState = nextStateAndProb[0]
+                nextProb = nextStateAndProb[1]
+                #get the reward for this current state
+                currReward = self.mdp.getReward(state, action, nextState)
+                #sum current utility with  the prob times the value of that state
+                currUtility += nextProb*self.values[nextState]
+              #if new utility is better, set that to utility and take its reward
+              if currUtility >= utility:
+                utility = currUtility
+                reward = currReward
+            #set value of the state to be the function (reward + (utility * gamma))
+            self.values[state] = reward + utility*self.discount
 
 
     def getValue(self, state):
@@ -78,16 +103,31 @@ class ValueIterationAgent(ValueEstimationAgent):
         """
         "*** YOUR CODE HERE ***"
         
+        
+        #initialze at 0
         value = 0
-        if self.mdp.isTerminal():
-          return getReward(state, action, state)
-          
+        #if self.mdp.isTerminal():
+          #return getReward(state, action, state)
+
+        #loop through the state and probability pairs for each transition state
         for nextStateAndProbs in self.mdp.getTransitionStatesAndProbs(state, action):
           currNextStateProb = nextStateAndProbs[1]
           currNextState = nextStateAndProbs[0]
-          currReward = self.mdp.getReward(state, action, currNextState)
-          print currReward
+          
+          #currReward = self.mdp.getReward(state, action, currNextState)
+          
+          #currValue = self.values[currNextState] * currNextStateProb
+          
+          #set current value to be the prob times the value of the state 
+          currValue = currNextStateProb * self.values[currNextState]
+          #if this new value is greater than our old, set value to the new value
+          if currValue >= value:
+            value = currValue
         return value
+        
+        #gamma = self.discount
+
+
 
     def computeActionFromValues(self, state):
         """
@@ -107,10 +147,30 @@ class ValueIterationAgent(ValueEstimationAgent):
         "*** YOUR CODE HERE ***"
 
         #values (initialized) prints {'TERMINAL_STATE': 0} {(0, 0): 0, 'TERMINAL_STATE': 0}
-        values = self.values
-        return self.values
+        #values = self.values
+        #return self.values
         #case for when a state has no available actions in an MDP 
+        if len(self.mdp.getPossibleActions(state)) == 0:
+          print "in base case, no possible actions"
+          return None
 
+        #initialize at 0 and None
+        value = 0
+        action = None
+
+        #loop through each action of all possible actions for the given state
+        for currAction in self.mdp.getPossibleActions(state):
+          print "currAction is", currAction
+          #set QValue using our computeQValueFromValues function
+          QValue = self.computeQValueFromValues(state, currAction)
+          print "QValue is", QValue
+          #If that new value is greater than our previous value, 
+          #we set value to this new value and action to that action
+          if QValue >= value:
+            value = QValue
+            action = currAction
+        print "best action is", action
+        return action
 
         #util.raiseNotDefined()
 
